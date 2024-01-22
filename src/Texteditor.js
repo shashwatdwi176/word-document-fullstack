954fa8e300c54315dbbe33a1e644f7dcbb64b3e6
shashwatdwi176 <dwivedishashwat176@gmail.com>
2024-01-22 23:55:43 +0530

    Added basic server setup

--- src/Texteditor.js
+++ src/Texteditor.js
@@ -1,7 +1,7 @@
-import React, { useCallback } from "react";
+import React, { useCallback , useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
-
+import {io} from 'socket.io-client';
 const TOOLBAR_OPTIONS = [
   [{header: [1,2,3,4,5,6,false]}],
   [{font : []}],
@@ -15,14 +15,27 @@ const TOOLBAR_OPTIONS = [
 ]
 
 const Texteditor = () => {
+    const [socket, setSocket] = useState();
+    const [quil,setQuil] = useState();
+    useEffect(() =>{
+     const s = io("http://localhost:3001")
+      setSocket(s)
+     return () => {
+      s.disconnect()
+     }
+    }, [])
 
+    useEffect(() => {
+      
+    })
     
     const wrapperRef = useCallback(wrapper => {
         if (wrapper == null) return
         wrapper.innerHTML =""
     const editor = document.createElement("div");
     wrapper.append(editor)
-    new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS }});
+    const q = new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS }});
+    setQuil(q)
    
   }, []);
   return <div className="container" ref={wrapperRef}></div>;
