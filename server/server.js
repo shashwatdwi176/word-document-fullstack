a83db855d12a245cbec9c28a61a9e9f009b13877
shashwatdwi176 <dwivedishashwat176@gmail.com>
2024-01-23 23:10:21 +0530

    Added websocket

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
-const io = require('socket.io')(3001 , {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ['GET' , 'POST']
-    }
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 })
 
-io.on("connection", socket => {
-    console.log("connected")
-})
+
+io.on("connection", (socket) => {
+  socket.on("send-changes", (delta) => {
+    socket.broadcast.emit("receive-changes",delta)
+  });
+});
--- src/Texteditor.js
+++ src/Texteditor.js
@@ -1,42 +1,65 @@
-import React, { useCallback , useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
-import {io} from 'socket.io-client';
+import { io } from "socket.io-client";
 const TOOLBAR_OPTIONS = [
-  [{header: [1,2,3,4,5,6,false]}],
-  [{font : []}],
-  [{list: "ordered"} , {list: "bullet"}],
-  ["bold","italic","underline"],
-  [{color:[]},{background:[]}],
-  [{script:"sub"},{script:"super"}],
-  [{align: []}],
-  ["image","blockquote","code-block"],
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
   ["clean"],
-]
+];
 
 const Texteditor = () => {
-    const [socket, setSocket] = useState();
-    const [quil,setQuil] = useState();
-    useEffect(() =>{
-     const s = io("http://localhost:3001")
-      setSocket(s)
-     return () => {
-      s.disconnect()
-     }
-    }, [])
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+    return () => {
+      s.disconnect();
+    };
+  }, []);
 
-    useEffect(() => {
-      
-    })
-    
-    const wrapperRef = useCallback(wrapper => {
-        if (wrapper == null) return
-        wrapper.innerHTML =""
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    const handler = delta => {
+      quill.updateContents(delta)
+    };
+    socket.on("receive-changes", handler);
+
+    return () => {
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return;
+      socket.emit("send-chnges", delta);
+    };
+    quill.on("text-change", handler);
+
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
+
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
     const editor = document.createElement("div");
-    wrapper.append(editor)
-    const q = new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS }});
-    setQuil(q)
-   
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    setQuill(q);
   }, []);
   return <div className="container" ref={wrapperRef}></div>;
 };
